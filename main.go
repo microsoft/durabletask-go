ab03cd0d3fb87abafb6887145e7fbab876ae639c
kaibocai <kaibocai@microsoft.com>
2024-01-12 08:49:25 -0600

    remove exporter

--- main.go
+++ main.go
@@ -7,11 +7,6 @@ import (
 	"log"
 	"net"
 
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/zipkin"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
 	"google.golang.org/grpc"
 
 	"github.com/microsoft/durabletask-go/backend"
@@ -28,17 +23,6 @@ func main() {
 	// Parse command-line arguments
 	flag.Parse()
 
-	// Tracing can be configured independently of the orchestration code.
-	tp, err := ConfigureZipkinTracing()
-	if err != nil {
-		log.Fatalf("Failed to create tracer: %v", err)
-	}
-	defer func() {
-		if err := tp.Shutdown(context.Background()); err != nil {
-			log.Fatalf("Failed to stop tracer: %v", err)
-		}
-	}()
-
 	grpcServer := grpc.NewServer()
 	worker := createTaskHubWorker(grpcServer, *dbFilePath, backend.DefaultLogger())
 	if err := worker.Start(ctx); err != nil {
@@ -54,30 +38,6 @@ func main() {
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-}
-
-func ConfigureZipkinTracing() (*trace.TracerProvider, error) {
-	// Inspired by this sample: https://github.com/open-telemetry/opentelemetry-go/blob/main/example/zipkin/main.go
-	exp, err := zipkin.New("http://localhost:9411/api/v2/spans")
-	if err != nil {
-		return nil, err
-	}
-
-	// NOTE: The simple span processor is not recommended for production.
-	//       Instead, the batch span processor should be used for production.
-	processor := trace.NewSimpleSpanProcessor(exp)
-	// processor := trace.NewBatchSpanProcessor(exp)
-
-	tp := trace.NewTracerProvider(
-		trace.WithSpanProcessor(processor),
-		trace.WithSampler(trace.AlwaysSample()),
-		trace.WithResource(resource.NewWithAttributes(
-			"durabletask.io",
-			attribute.KeyValue{Key: "service.name", Value: attribute.StringValue("sample-app")},
-		)),
-	)
-	otel.SetTracerProvider(tp)
-	return tp, nil
 }
 
 func createTaskHubWorker(server *grpc.Server, sqliteFilePath string, logger backend.Logger) backend.TaskHubWorker {
