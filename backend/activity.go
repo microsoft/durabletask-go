6b3830ca73ea39d3d6f7c28ad1ce9b0d9d9050d5
Cassandra Coyle <cassie@diagrid.io>
2025-06-18 15:35:24 -0500

    cleanup
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- backend/activity.go
+++ backend/activity.go
@@ -50,15 +50,6 @@ func (p *activityProcessor) ProcessWorkItem(ctx context.Context, awi *ActivityWo
 		return fmt.Errorf("%v: invalid TaskScheduled event", awi.InstanceID)
 	}
 
-	if awi.NewEvent.Router == nil {
-		awi.NewEvent.Router = &protos.TaskRouter{}
-	}
-
-	if eventRouter := awi.NewEvent.GetRouter(); eventRouter != nil {
-		awi.NewEvent.Router.Source = eventRouter.GetSource()
-		awi.NewEvent.Router.Target = eventRouter.GetTarget()
-	}
-
 	// Create span as child of spanContext found in TaskScheduledEvent
 	ctx, err := helpers.ContextFromTraceContext(ctx, ts.ParentTraceContext)
 	if err != nil {
@@ -83,17 +74,6 @@ func (p *activityProcessor) ProcessWorkItem(ctx context.Context, awi *ActivityWo
 			span.SetStatus(codes.Error, err.Error())
 		}
 		return err
-	}
-
-	if result != nil {
-		if result.Router == nil {
-			result.Router = &protos.TaskRouter{}
-		}
-		// Copy Router info from the NewEvent to preserve cross-app routing context
-		if awi.NewEvent.Router != nil {
-			result.Router.Source = awi.NewEvent.Router.Source
-			result.Router.Target = awi.NewEvent.Router.Target
-		}
 	}
 
 	awi.Result = result
