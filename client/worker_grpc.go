55150ae4f9abd196bdf75a59cab2de95b03a6751
salaboy <Salaboy@gmail.com>
2025-08-11 12:03:17 +0100

    Update worker_grpc.go
    Signed-off-by: salaboy <Salaboy@gmail.com>

--- client/worker_grpc.go
+++ client/worker_grpc.go
@@ -178,7 +178,7 @@ func (c *TaskHubGrpcClient) processActivityWorkItem(
 	var ptc *protos.TraceContext = req.ParentTraceContext
 	ctx, err := helpers.ContextFromTraceContext(ctx, ptc)
 	if err != nil {
-		fmt.Printf("%v: failed to parse trace context: %v", req.Name, err)
+		c.logger.Warn("%v: failed to parse trace context: %v", req.Name, err)
 	}
 
 	event := &protos.HistoryEvent{
